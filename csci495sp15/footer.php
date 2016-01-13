6bba50fdd7dbbfeceb1af71f4e779102e74af4cb
RomanRA <raroman@g.coastal.edu>
2016-01-13 15:52:46 -0500

    Full CMS
    All files

--- csci495sp15/footer.php
+++
@@ -1,6 +0,0 @@
-
-<div class = "navbar navbar-default navbar-fixed-bottom">
-    <div class = "container">
-        <p class = "navbar-text pull-right">Copyright CSCI Department</p>
-    </div>
-</div>
